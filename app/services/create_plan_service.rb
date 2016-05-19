be654373c68ab91473c4cc0dd57f832e3431ca6a
don Onwunum <me@teopeurt.com>
2016-05-19 16:37:38 +0100

    initial commit

--- app/services/create_plan_service.rb
+++ app/services/create_plan_service.rb
@@ -1,21 +1,21 @@
 class CreatePlanService
   def call
-    p1 = Plan.where(name: 'Platinum').first_or_initialize do |p|
-      p.amount = 2900
+    p1 = Plan.where(name: 'Gold').first_or_initialize do |p|
+      p.amount = 1999
       p.interval = 'month'
-      p.stripe_id = 'platinum'
+      p.stripe_id = 'goldinvoice'
     end
     p1.save!(:validate => false)
-    p2 = Plan.where(name: 'Gold').first_or_initialize do |p|
-      p.amount = 1900
+    p2 = Plan.where(name: 'Silver').first_or_initialize do |p|
+      p.amount = 999
       p.interval = 'month'
-      p.stripe_id = 'gold'
+      p.stripe_id = 'silverinvoice'
     end
     p2.save!(:validate => false)
-    p3 = Plan.where(name: 'Silver').first_or_initialize do |p|
-      p.amount = 900
+    p3 = Plan.where(name: 'Bronze').first_or_initialize do |p|
+      p.amount = 599
       p.interval = 'month'
-      p.stripe_id = 'silver'
+      p.stripe_id = 'bronzeinvoice'
     end
     p3.save!(:validate => false)
   end
